2ea7027ac380a36e133e8ac0ed449c377a3fc818
Oluwafemi Awoyemi <femikola23@gmail.com>
2022-01-24 15:53:35 +0400

    Insert data in data.sql as per reviewer suggestion

--- data.sql
+++ data.sql
@@ -91,3 +91,9 @@ INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id FROM 
 INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'Stephanie Mendez'), '2020-05-24');
 
 INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES ((SELECT id FROM animals WHERE name = 'Blossom'), (SELECT id FROM vets WHERE name = 'William Tatcher'), '2021-01-11');
+
+
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
