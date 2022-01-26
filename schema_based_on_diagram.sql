100981229d977427fa67218854b1f0a5248bf0e6
Timowest12 <timowester@gmail.com>
2022-01-26 11:43:02 +0100

    Add taables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,1 +1,22 @@
 
+CREATE TABLE patients (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(256),
+    date_of_birth DATE
+);
+
+CREATE TABLE treaments (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(256),
+    name VARCHAR(256)
+);
+
+CREATE TABLE medical_histories (
+    id SERIAL PRIMARY KEY,
+    admitted_at TIMESTAMP,
+   patient_id INT,
+   status VARCHAR(256),
+   CONSTRAINT fk_patient FOREIGN KEY(patient_id) REFERENCES patients(id)  
+);
+
+
